831338623428020d04ba7ae312e79533f420a390
bcliang <brad@bliang.com>
2019-03-11 06:55:07 -0700

    unittests and test data for new classes

---
+++ tests/test_chronoamperometry.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import gamry_parser as parser
+import unittest
+
+class TestChronoAmperometry(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_use_datetime(self):
+        gp = parser.ChronoAmperometry(filename='tests/chronoa_data.dta', to_timestamp=False)
+        gp.load()
+        curve = gp.get_curve_data()
+        # 'T' should return elapsed time in seconds
+        self.assertEqual(curve['T'][0], 0)
+        self.assertEqual(curve['T'][-1], 270)
+
+        gp = parser.ChronoAmperometry(filename='tests/chronoa_data.dta', to_timestamp=True)
+        gp.load()
+        curve = gp.get_curve_data()
+        # 'T' should return datetime objects
+        self.assertEqual(curve['T'][0], pd.to_datetime('3/10/2019 12:00:00'))
+        self.assertEqual(curve['T'][-1], pd.to_datetime('3/10/2019 12:04:30'))
+
+    def test_getters(self):
+        gp = parser.ChronoAmperometry(filename='tests/chronoa_data.dta')
+        gp.load()
+        curve = gp.get_curve_data()
+        self.assertTrue((curve.columns == ['T', 'Vf', 'Im']).all())
---
+++ tests/test_cyclicvoltammetry.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import gamry_parser as parser
+import unittest
+
+class TestCyclicVoltammetry(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_is_loaded(self):
+        gp = parser.CyclicVoltammetry()
+        # should raise exception if file not loaded
+        self.assertRaises(AssertionError, gp.get_curve_count)
+        self.assertRaises(AssertionError, gp.get_curve_data, 2)
+        self.assertRaises(AssertionError, gp.get_curves)
+        self.assertRaises(AssertionError, gp.get_experiment_type)
+
+    def test_getters(self):
+        gp = parser.CyclicVoltammetry(filename='tests/cv_data.dta')
+        gp.load()
+        vrange = gp.get_v_range()
+        self.assertEqual(vrange[0], 0.49)
+        self.assertEqual(vrange[-1], 0.50)
+
+        curve = gp.get_curve_data(1)
+        self.assertTrue((curve.columns == ['Vf', 'Im']).all())
