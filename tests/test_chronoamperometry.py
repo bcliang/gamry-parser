680bf393426d82c8dac7af0037b1e9b490d9fd4f
bcliang <brad@bliang.com>
2019-03-14 14:11:14 -0700

    tests for new methods in gamry_parser.chronoa

--- tests/test_chronoamperometry.py
+++ tests/test_chronoamperometry.py
@@ -2,6 +2,7 @@ import pandas as pd
 import gamry_parser as parser
 import unittest
 
+
 class TestChronoAmperometry(unittest.TestCase):
     def setUp(self):
         pass
@@ -23,6 +24,12 @@ class TestChronoAmperometry(unittest.TestCase):
 
     def test_getters(self):
         gp = parser.ChronoAmperometry(filename='tests/chronoa_data.dta')
+        self.assertRaises(AssertionError, gp.get_sample_time)
+        self.assertRaises(AssertionError, gp.get_sample_count)
+
         gp.load()
         curve = gp.get_curve_data()
         self.assertTrue((curve.columns == ['T', 'Vf', 'Im']).all())
+
+        self.assertEqual(gp.get_sample_time(), 30)
+        self.assertEqual(gp.get_sample_count(), 10)
