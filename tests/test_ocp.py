b9475eda0fafdc8b9bd33fd7501659aae7c42a9a
Brad Liang <brad@bliang.com>
2020-05-30 07:37:28 -0700

    test: should fail CI workflow

--- tests/test_ocp.py
+++ tests/test_ocp.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import gamry_parser as parser
 import unittest
-from pandas.util.testing import assert_frame_equal
-
+from pandas.testing import assert_frame_equal
 
 class TestOpenCircuit(unittest.TestCase):
     def setUp(self):
