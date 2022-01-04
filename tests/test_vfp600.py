dc0550d143685cb5225f8cc33964abedc5b6150c
Brad Liang <brad@bliang.com>
2022-01-04 11:58:24 -0800

    update tests for subclass props

--- tests/test_vfp600.py
+++ tests/test_vfp600.py
@@ -9,21 +9,25 @@ class TestVFP600(unittest.TestCase):
     def test_load(self):
         gp = parser.VFP600()
         self.assertFalse(gp.loaded)
+        self.assertEqual(gp.sample_time, 0)
+        self.assertEqual(gp.sample_count, 0)
+        self.assertEqual(gp.curve_count, 0)
 
         gp.load("tests/vfp600_data.dta")
         self.assertEqual(gp.fname, "tests/vfp600_data.dta")
-        self.assertTrue("VFP600" in gp.get_header()["TAG"])
+        self.assertEqual(gp.experiment_type, "VFP600")
+        
         # data file acq frequency = 15hz
-        self.assertEqual(gp.get_sample_time(), 1 / 15)
-        self.assertEqual(gp.get_curve_count(), 1)
-        self.assertEqual(gp.get_sample_count(), 20)
+        self.assertEqual(gp.sample_time, 1 / 15)
+        self.assertEqual(gp.curve_count, 1)
+        self.assertEqual(gp.sample_count, 20)
         self.assertTrue(gp.loaded)
 
     def test_getters(self):
         gp = parser.VFP600()
         gp.load("tests/vfp600_data.dta")
 
-        curve = gp.get_curve_data()
+        curve = gp.curve()
         self.assertTrue((curve.columns == ["T", "Voltage", "Current"]).all())
 
         self.assertEqual(curve["T"][0], 0)
